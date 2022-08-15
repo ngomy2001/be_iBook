6d34faa8ee6c9fe2467a73ae4301187983d96cea
ngomy2001 <ngomy2001@gmail.com>
2022-08-15 12:17:33 +0700

    update-account/role-model

--- database/models/RoleModel.js
+++ database/models/RoleModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const { LIBRARIAN_ROLE, MEMBER_ROLE } = require('../../Constants/role');
 const RoleSchema = new Schema(
   {
     name: {
