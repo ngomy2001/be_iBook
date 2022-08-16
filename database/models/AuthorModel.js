395a85f613af56b7946575af1292a934292122af
ngomy2001 <ngomy2001@gmail.com>
2022-08-16 13:04:22 +0700

    setup-author-model

--- database/models/AuthorModel.js
+++ database/models/AuthorModel.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const AuthorSchema = new Schema(
+  {
+    firstName: String,
+    lastName: String,
+    description: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+const Author = mongoose.model('Author', AuthorSchema);
+module.exports = Author;
