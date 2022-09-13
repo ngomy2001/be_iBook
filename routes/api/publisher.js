9c234eac73e3837e37197deb8f34b0efaf76f7f1
ngomy2001 <ngomy2001@gmail.com>
2022-09-14 02:11:44 +0700

    publisher-api-docs

--- routes/api/publisher.js
+++ routes/api/publisher.js
@@ -6,9 +6,48 @@ const {
   updatePublisherInfo,
   deletePublisherInfor,
 } = require('../../Controllers/PublisherController');
-
+/**
+ * @swagger
+ * /api/publisher:
+ *   get:
+ *     summary: Retrieve a list of publishers in the system
+ *     description: Retrieve a list of publishers from database.
+ *     responses:
+ *       200:
+ *         description: OK.
+ */
 router.get('/', getAllPublishers);
+/**
+ * @swagger
+ * /api/publisher/createPublisher:
+ *   post:
+ *     summary: Add a new publisher to the system.
+ *     description: Add a new publisher to the system.
+ *     responses:
+ *       201:
+ *         description: Create successfully.
+ */
 router.post('/createPublisher', createPublisher);
+/**
+ * @swagger
+ * /api/publisher/{id}:
+ *   put:
+ *     summary: Update an publisher info.
+ *     description: Change an information of a publisher.
+ *     responses:
+ *       200:
+ *         description: OK.
+ */
 router.put('/:id', updatePublisherInfo);
+/**
+ * @swagger
+ * /api/publisher/{id}:
+ *   delete:
+ *     summary: Delete a publisher info.
+ *     description: Remove an information of a publisher from database.
+ *     responses:
+ *       200:
+ *         description: OK.
+ */
 router.delete('/:id', deletePublisherInfor);
 module.exports = router;
